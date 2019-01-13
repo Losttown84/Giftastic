efe9b3865a6b7e16375df03ec48607651471b169
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-01-13 10:00:58 -0600

    added pseudocode in js file and first vars for subject matter and called the url

---
+++ assets/app.js
@@ -0,0 +1,25 @@
+// pseudocode
+// call giphy api
+// retrieve the data from giphy
+// start with an initial array of subjects for giphy to display
+// add in gif rating
+// set-up holders for gifs to display
+// once data is retrieved, display them on webpage
+// user should be able to create new buttons for additional gifs
+
+var comedy = ["George Carlin", "Ali Wong", "Aziz Ansari", "Tina Fey", "Titus Burgess"];
+
+function displayGif(){
+
+    var gif = $(this).attr('data-name');
+    var queryURL = "https://api.giphy.com/v1/gifs/trending?api_key=dc6zaTOxFJmzC";
+}
+
+var queryURL = "https://api.giphy.com/v1/gifs/search" + gif +  "&api_key=Pu6cLOh06nWg3HRDUMoZXAj7Y9M275kV";
+
+$.ajax({
+  url: queryURL,
+  method: "GET"
+}).then(function(response) {
+  console.log(response);
+});
